a737006eead9d28357a0ff41e36028aff325f6ee
Corey <Coreyenon@gmail.com>
2014-10-15 16:34:26 -0400

    Merging!

--- PrintMyName.py
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/python
-print("Rutter, Corey")
-print("Version control is essential to life as we know it")
--- README.md
+++
@@ -1,4 +0,0 @@
-CS2350
-======
-
-A resource for learning about version control
